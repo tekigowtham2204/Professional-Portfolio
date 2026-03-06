a3703e2a00f8e38514fa396d8bae80fc35089781
gowtham bhaskar <gowthamteki23@lpu.in>
2026-03-06 13:44:05 +0530

    Update vite.config.ts with GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Professional-Portfolio/',
   plugins: [
     react(),
     tailwindcss(),
